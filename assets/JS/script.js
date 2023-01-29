90ba64de8a63061399c0f5af3ea58a0461197280
ErickNoGit <erickalves20199@outlook.com>
2023-01-29 12:50:00 -0300

    alteração no header e html

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,20 @@
+// botão mobile do header
 const botaoMobile = document.querySelector("#btn-mobile");
 
 function ativarMenu(event) {
+  if (event.type === 'touchstart') event.preventDefoult();
   const acesso = document.getElementById("acessos");
   acesso.classList.toggle('ativado');
+
+  const active = acesso.classList.contains('ativado');
+  event.currentTarget.setAttribute('aria-expanded', active);
+
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
 }
+
 botaoMobile.addEventListener("click", ativarMenu);
+botaoMobile.addEventListener("touchstart", ativarMenu);
